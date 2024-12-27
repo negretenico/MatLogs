4b20832d2b5e8e7bac45602f4ec2a0e6f2095039
Nico <negretenico@gmail.com>
2024-12-27 10:21:07 -0500

    adding the required information and details to connect to firebase

---
+++ src/config/firebaseConfig.ts
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import {initializeApp} from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCZvyUB04MC0ddRFBX-5jx4FgKHYQXEy5I", // fine to be public since ths just identifies the project on the google server
+    authDomain: "matlogs.firebaseapp.com",
+    databaseURL: "https://matlogs-default-rtdb.firebaseio.com",
+    projectId: "matlogs",
+    storageBucket: "matlogs.firebasestorage.app",
+    messagingSenderId: "636230050985",
+    appId: "1:636230050985:web:c855466372efdc707975b8",
+    measurementId: "G-8QSXGT6NJL"
+};
+
+// Initialize Firebase
+const config = initializeApp(firebaseConfig);
+export default config;
---
+++ src/query/keys.ts
@@ -0,0 +1,6 @@
+export const QUERY_KEYS = {
+    get :{
+        all: ['all'],
+        individual: ['individual'],
+    }
+}
