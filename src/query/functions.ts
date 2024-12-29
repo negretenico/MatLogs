f88494b29b9a52025effaedf437ae2ccd256a20f
Nico <negretenico@gmail.com>
2024-12-29 15:48:46 -0500

    Small changes for deployments; renaming deployment file

--- src/query/functions.ts
+++ src/query/functions.ts
@@ -1,14 +1,14 @@
-import {all} from "./get/all";
-import {individual} from "./get/individual";
+import { all } from './get/all';
+import { individual } from './get/individual';
 
-const env = process.env.REACT_APP_ENV || "development"; // Default to "development"
+const env = process.env.REACT_APP_ENV || 'development'; // Default to "development"
 
 const withEnvironment = (functionsByEnv: Record<string, Function>) => {
-    return functionsByEnv[env] || functionsByEnv["default"];
+  return functionsByEnv[env] || functionsByEnv['default'];
 };
 export const QUERY_FUNCTIONS = {
-    get: {
-        all: withEnvironment(all),
-        individual: (key: string) => withEnvironment(individual)(key),
-    }
-}
+  get: {
+    all: withEnvironment(all),
+    individual: (key: string) => withEnvironment(individual)(key),
+  },
+};
